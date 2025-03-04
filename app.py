450f42d35088e5ed7fa7142ef3b088429d4ec754
steiner93 <i.steiner.93@gmail.com>
2025-03-04 15:16:14 +0100

    add: app 2

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
 @socketio.on("message")
 def handle_message(msg):
     print(f"Message: {msg}")
-    socketio.emit("message", msg, broadcast=True)
+    socketio.send(msg, broadcast=True)  # Broadcasting the message
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
